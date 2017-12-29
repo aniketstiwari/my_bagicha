db2ab5cfd9cc3ef3396cf41cde4706b5afe2182b
Aniket Shivam Tiwari <aniket.tiw@gmail.com>
2017-12-29 12:40:33 +0530

    added

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module MyBagicha
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    Rails.application.config.assets.precompile += %w( *.js *.css )
+
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
