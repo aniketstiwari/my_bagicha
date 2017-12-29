a02ef8e9592a6d0b2f97059d262794b2a43e7739
Aniket Shivam Tiwari <aniket.tiw@gmail.com>
2017-12-29 13:12:30 +0530

    error resolved

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module MyBagicha
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    #config.load_defaults 5.1
     Rails.application.config.assets.precompile += %w( *.js *.css )
 
 
